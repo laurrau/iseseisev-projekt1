5e26ec20a14b0e9cfb956ac986328efb8b0a32e4
Lauri Raus <43170969+laurrau@users.noreply.github.com>
2019-06-03 13:20:08 +0300

    Lauri Raus

---
+++ javascript.js
@@ -0,0 +1,109 @@
+
+var modal = document.getElementById("myModal");
+
+// nupp käivitab
+var btn = document.getElementById("myBtn");
+
+//<span> element sulgeb modali
+var span = document.getElementsByClassName("close")[0];
+
+// nupu peale vajutades käivitab
+btn.onclick = function() {
+  modal.style.display = "block";
+};
+
+// x sulgeb akna
+span.onclick = function() {
+  modal.style.display = "none";
+};
+
+// Väljaspool kasti vajutades sulgeb
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+
+//bänner
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 5000); //slideshow kiirus
+}
+
+//fade1
+var el = document.getElementById("div1");
+
+function fadeIn(el, time) {
+  el.style.opacity = 0;
+
+  var last = +new Date();
+  var tick = function() {
+    el.style.opacity = +el.style.opacity + (new Date() - last) / time;
+    last = +new Date();
+
+    if (+el.style.opacity < 1) {
+      (window.requestAnimationFrame && requestAnimationFrame(tick)) || setTimeout(tick, 16);
+    }
+  };
+
+  tick();
+}
+
+fadeIn(el, 2000);
+
+//fade2
+var el = document.getElementById("div2");
+
+function fadeIn(el, time) {
+  el.style.opacity = 0;
+
+  var last = +new Date();
+  var tick = function() {
+    el.style.opacity = +el.style.opacity + (new Date() - last) / time;
+    last = +new Date();
+
+    if (+el.style.opacity < 1) {
+      (window.requestAnimationFrame && requestAnimationFrame(tick)) || setTimeout(tick, 16);
+    }
+  };
+
+  tick();
+}
+
+fadeIn(el, 2400);
+
+//fade3
+var el = document.getElementById("div3");
+
+function fadeIn(el, time) {
+  el.style.opacity = 0;
+
+  var last = +new Date();
+  var tick = function() {
+    el.style.opacity = +el.style.opacity + (new Date() - last) / time;
+    last = +new Date();
+
+    if (+el.style.opacity < 1) {
+      (window.requestAnimationFrame && requestAnimationFrame(tick)) || setTimeout(tick, 16);
+    }
+  };
+
+  tick();
+}
+
+fadeIn(el, 2600);
